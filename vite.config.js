babddbc3114b643a6074eb7b6248ff232c4cbc57
shahadatpi <here.is.shahadat@gmail.com>
2023-08-19 20:23:21 +0600

    Vite Config Corrected

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,31 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
-})
+  plugins: [
+    vue(),
+    laravel({
+      input: ['resources/js/app.js'],
+      refresh: true,
+    }),
+    i18n(),
+  ],
+
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+      ziggy: path.resolve('vendor/tightenco/ziggy/dist/vue.es.js'),
+
+    },
+  },
+  build: {
+    rollupOptions: {
+      output:{
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
+});
